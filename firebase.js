8dcb8e28f31eae188c2897d9d6f6c201180e94a8
PSXgamingytPSX <Kayden.williams91812@gmail.com>
2026-06-09 17:06:46 +0100

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,9 @@
+
+  apiKey: "AIzaSyDQ8LGT0d8kpF4vPeTpTs-otEca688J8P8",
+  authDomain: "testerforall-9ea12.firebaseapp.com",
+  projectId: "testerforall-9ea12",
+  storageBucket: "testerforall-9ea12.firebasestorage.app",
+  messagingSenderId: "434915664367",
+  appId: "1:434915664367:web:33ff6446c1febdebbe961a",
+  measurementId: "G-S56N08LS6F"
+};
