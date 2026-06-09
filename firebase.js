fa2deae878dc225dbc85e59d9b1f92e0baaa6994
PSXgamingytPSX <Kayden.williams91812@gmail.com>
2026-06-09 17:08:51 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,8 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
+const firebaseConfig = {
   apiKey: "AIzaSyDQ8LGT0d8kpF4vPeTpTs-otEca688J8P8",
   authDomain: "testerforall-9ea12.firebaseapp.com",
   projectId: "testerforall-9ea12",
@@ -7,3 +11,8 @@
   appId: "1:434915664367:web:33ff6446c1febdebbe961a",
   measurementId: "G-S56N08LS6F"
 };
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
